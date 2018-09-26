f811b53411f6d27e03b66799b7d6025129c87ed5
Rose <rose1264@gmail.com>
2018-09-26 14:16:55 -0400

    neighbourhood see data

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ActiveRecord::Base
+  
   belongs_to :host, class_name: 'User'
   belongs_to :neighbourhood
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+manhattanNeighbourhoods = ["Alphabet City",
+"Battery Park City",
+"Carnegie Hill",
+"Chelsea",
+"Chinatown",
+"East Harlem",
+"East Village",
+"Financial District",
+"Flatiron District",
+"Gramercy Park",
+"Greenwich Village",
+"Harlem",
+"Hell's Kitchen/Clinton",
+"Inwood",
+"Kips Bay",
+"Lincoln Square",
+"Lower East Side",
+"Manhattan Valley",
+"Midtown East",
+"Midtown West",
+"Morningside Heights",
+"Murray Hill",
+"NoLita/Little Italy",
+"Roosevelt Island",
+"SoHo",
+"Tribeca",
+"Upper East Side",
+"Upper West Side",
+"Washington Heights",
+"West Village"]
+
+
+City.create!(name:"NYC")
+manhattanNeighbourhoods.each {|neighbourhood| Neighbourhood.create!(name: neighbourhood, city_id: 1)}
